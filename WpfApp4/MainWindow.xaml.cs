f61e8731c25ffad9a092ed740d6fc3b1fc0e53dc
Daniel Kuglitsch <Daniel.Kuglitsch@gmail.com>
2020-02-17 10:48:16 +0100

    Add project files.

---
+++ WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+
+        public ObservableCollection<TestItem> Items = new ObservableCollection<TestItem>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            lvTest.ItemsSource = Items;
+            lvTest1.ItemsSource = Items;
+            lvTest2.ItemsSource = Items;
+
+            foreach (var v in Enumerable.Range(1, 50))
+            {
+                Items.Add(new TestItem("Test " + v));
+            }
+        }
+    }
+}
---
+++ WpfApp4/TestItem.cs
@@ -0,0 +1,17 @@
+﻿namespace WpfApp4
+{
+    public class TestItem
+    {
+        public string Content { get; }
+
+        public TestItem(string content)
+        {
+            Content = content;
+        }
+
+        public override string ToString()
+        {
+            return Content;
+        }
+    }
+}
